24280c99a360521e5699facea31e1663a0bcefd0
Ik Sung Choi <ikchoi0@gmail.com>
2022-04-20 17:41:05 +0900

    Inital commit with all my work so far

---
+++ if-statement.js
@@ -0,0 +1,5 @@
+var fruits = "orange";
+if(fruits.length > 5){
+  console.log("The fruit name has more than five characters.");
+} else 
+  console.log("The fruit name has five characters or less.");
